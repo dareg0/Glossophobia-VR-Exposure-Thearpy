4ec1a0cf64ab84dcf21affdc5c32b0dc2dc06805
Zhiling Hu <zh347@cornell.edu>
2019-11-18 00:40:00 -0500

    timer done (not tested); psd file created, psd importer doesn't work

---
+++ Glossophobia/Assets/TimerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public Text timerMinutes;
+    public Text timerSeconds;
+    public Text timerSeconds100;
+
+    private float startTime;
+    private float stopTime;
+    private float timerTime;
+    private bool isRunning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerReset();
+    }
+
+    public void TimerStart()
+    {
+        if (!isRunning)
+        {
+            Debug.Log("START");
+            isRunning = true;
+            startTime = Time.time;
+        }
+    }
+
+    public void TimerStop()
+    {
+        if (isRunning)
+        {
+            Debug.Log("STOP");
+            isRunning = false;
+            stopTime = timerTime;
+        }
+    }
+
+    public void TimerReset()
+    {
+        Debug.Log("RESET");
+        timerMinutes.text = timerSeconds.text = timerSeconds100.text = "00";
+    }
+
+    private void Update()
+    {
+        timerTime = stopTime + (Time.time - startTime);
+        int minutesInt = (int)timerTime / 60;
+        int secondsInt = (int)timerTime % 60;
+        int seconds100Int = (int)(Mathf.Floor((timerTime - (secondsInt + minutesInt * 60)) * 100));
+
+        if (isRunning)
+        {
+            timerMinutes.text = (minutesInt < 10) ? "0" + minutesInt: minutesInt.ToString();
+            timerSeconds.text = (secondsInt < 10) ? "0" + secondsInt : secondsInt.ToString();
+            timerSeconds100.text = (seconds100Int < 10) ? "0" + seconds100Int : seconds100Int.ToString();
+        }
+    }
+}
